282b55d95ce62f8631d41923f52be443f38545ee
Alison Rampton <alison.rampton@gmail.com>
2024-02-02 16:56:05 -0700

    Started reformatting individual works page

--- app/catalog/works/page.tsx
+++ app/catalog/works/page.tsx
@@ -1,18 +1,12 @@
 //This is NOT the main seach page, just a placeholder for making work pages later hopefully
 
-import { ResultPageSkeleton } from "@/app/ui/skeletons";
-
 export default async function Page({
   searchParams,
 }: {
   searchParams?: {
-    query?: string;
-    page?: string;
+    workID: string;
   };
 }) {
-  return (
-    <div className="w-full">
-      <ResultPageSkeleton />
-    </div>
-  );
+  // const work = await fetchIndividualWork(workID);
+  return <div className="w-full">{searchParams?.workID}</div>;
 }
